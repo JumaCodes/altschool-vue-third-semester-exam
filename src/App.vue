1d3a97cc1cda758c3939d0064f4766b627f6565b
CodexLegacy <73387073+CodexLegacy@users.noreply.github.com>
2023-03-18 15:21:18 +0100

    Api is not working

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
+// I believe eslint has a problem with my code but i dont want to turn it off.
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your jumama's Third Semester Exam"/>
+  <div>
+    <UserInfo />
+    <!-- <RouterView /> -->
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+// import { RouterView } from "vue-router";
+import UserInfo from './components/UserInfo.vue'
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Roboto+Condensed");
+html,
+body {
+  font-family: "Roboto", sans-serif;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Roboto", sans-serif;
 }
 </style>
